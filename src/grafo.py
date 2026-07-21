66e76230fb28bc68781b04980b406d4b6b6e5b14
Viviana Hurtado <vivianahurtado013@gmail.com>
2026-07-20 19:36:21 -0500

    Actualización grafo.py y creación del grafo

--- src/grafo.py
+++ src/grafo.py
@@ -21,6 +21,24 @@ def generar_grafo():
     
     return grafo.compile()
 
+def generar_visualizacion(grafo):
+    """Genera y guarda la imagen del grafo dentro de la carpeta assets."""
+    try:
+        base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        assets_dir = os.path.join(base_dir, "assets")
+        
+        os.makedirs(assets_dir, exist_ok=True)
+        
+        ruta_guardado = os.path.join(assets_dir, "grafo_flujo.png")
+        
+        png_data = grafo.get_graph().draw_mermaid_png()
+        with open(ruta_guardado, "wb") as f:
+            f.write(png_data)
+            
+        print(f"Imagen del grafo generada correctamente en: {ruta_guardado}")
+    except Exception as e:
+        print(f"Error al generar la imagen: {e}")
+
 def ejecutar_grafo(pregunta: str)-> str:
     global grafo_compilado
     if grafo_compilado is None:
@@ -30,9 +48,7 @@ def ejecutar_grafo(pregunta: str)-> str:
     respuesta=grafo_compilado.invoke({"pregunta": pregunta})
     
     print("Respuesta generada:", respuesta.get("respuesta", "No se pudo generar una respuesta."))
+    
+    #generar_visualizacion(grafo_compilado)
     return respuesta.get("respuesta", "No se pudo generar una respuesta.")
 
-if __name__ == "__main__":
-    pregunta = "¿Cuál es el tiempo estimado de entrega para un pedido realizado en la Ciudad de México?"
-    respuesta = ejecutar_grafo(pregunta)
-    print("Respuesta final:", respuesta)
